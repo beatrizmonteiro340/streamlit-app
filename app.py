74979046cdda133a1003f1ed09fbb0344f9d8763
beatrizmonteiro340 <beatrizmonteiro220403@gmail.com>
2026-05-15 15:14:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -254,8 +254,8 @@ elif st.session_state.pagina == "uma_bobineTAGPRICE":
         producao_total = (ultimacaixa-primeiracaixa+1)*unidades_por_caixa +unidades_soltas_primeira+unidades_soltas_ultima
 
         # Total de rótulos válidos
-        total_rotulos = (primeiro_rotulo - ultimo_rotulo + 1) - inutilizados - amostras
-
+        total_rotulos = (primeiro_rotulo - ultimo_rotulo + 1)
+        total_rotulosvalidos= (primeiro_rotulo - ultimo_rotulo + 1) - inutilizados - amostras
         soma_rejeitados = inutilizados + amostras
 
         #Resultados
